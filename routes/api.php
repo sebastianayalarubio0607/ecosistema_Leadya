8f6cfa7d3c4848ed913783a11bc26178407f7b71
sebastianayalarubio0607 <sebastianayalarubio@gmail.com>
2025-05-04 10:56:53 -0500

    sacar resect de cistumers

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,13 @@ use App\Http\Controllers\LeadIntegrationController;
 use App\Http\Middleware\ApiAuthMiddleware;
 
 //Route::apiResource('customers', CustomerController::class);
+Route::post('/customers/{id}/regenerate-token', [CustomerController::class, 'regenerateToken']);
 
 // Usamos el middleware por alias registrado en Kernel
 Route::middleware([ApiAuthMiddleware::class])->group(function () {
     
     Route::apiResource('customers', CustomerController::class);
-    Route::post('/customers/{id}/regenerate-token', [CustomerController::class, 'regenerateToken']);
+    
     Route::apiResource('integration-types', IntegrationTypeController::class);
     Route::apiResource('leads', LeadController::class);
     Route::apiResource('integrations', IntegrationController::class);
