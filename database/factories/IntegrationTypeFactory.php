0606962fa382a1860aa4e07f70a73f85e1376ec3
sebastianayalarubio0607 <sebastianayalarubio@gmail.com>
2025-09-30 17:47:31 -0500

    nueva

--- database/factories/IntegrationTypeFactory.php
+++ database/factories/IntegrationTypeFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\IntegrationType>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Integrationtype>
  */
-class IntegrationTypeFactory extends Factory
+class Integrationtype extends Factory
 {
     public function definition(): array
     {
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => 'sebastian.ayala@leadsya.com',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('Temp.1122'),
             'remember_token' => Str::random(10),
